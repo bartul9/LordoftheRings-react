2a5f9559c2ca554876d2890af306bd66bb04729e
Luka <bartul123@outlook.com>
2021-02-19 18:05:15 +0100

    Create Message.js

---
+++ src/Components/Message.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Message.css";
+
+export default class Message extends Component {
+  render() {
+    return (
+      <div className="Message">
+        <h2>Get into adventure, you'l never forget</h2>
+      </div>
+    );
+  }
+}
