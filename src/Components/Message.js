8bc07d37506c0b5b11893b7cd80ebde93ac6e1a9
Luka <bartul123@outlook.com>
2021-02-19 23:06:38 +0100

    Update Message.js

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -5,7 +5,15 @@ export default class Message extends Component {
   render() {
     return (
       <div className="Message">
-        <h2>Get into adventure, you'l never forget</h2>
+        <p>
+          The Lord of the Rings is an epic high fantasy novel by the English
+          author and scholar J. R. R. Tolkien. Set in Middle-earth, the world at
+          some distant time in the past, the story began as a sequel to
+          Tolkien's 1937 children's book The Hobbit, but eventually developed
+          into a much larger work. Written in stages between 1937 and 1949, The
+          Lord of the Rings is one of the best-selling books ever written, with
+          over 150 million copies sold.
+        </p>
       </div>
     );
   }
