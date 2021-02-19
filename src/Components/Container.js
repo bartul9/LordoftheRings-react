eee5540d40dd5fcbf1f6fb2bd0e2224f8d4bcd1b
Luka <bartul123@outlook.com>
2021-02-19 18:05:22 +0100

    Create Container.js

---
+++ src/Components/Container.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "./Container.css";
+import axios from "axios";
+import Movies from "./Movies";
+import Message from "./Message";
+
+export default class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      active: false,
+    };
+    this.fetchData = this.fetchData.bind(this);
+    this.changeActive = this.changeActive.bind(this);
+  }
+
+  async fetchData() {
+    const movies = await axios.get("https://the-one-api.dev/v2/movie", {
+      headers: { Authorization: `Bearer ${"9xc-nEAjaOlSvV8iSCfU"}` },
+    });
+    console.log(movies);
+    this.setState({
+      movies: movies.data.docs,
+    });
+  }
+
+  changeActive() {
+    this.setState({
+      active: !this.state.active,
+    });
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  render() {
+    return (
+      <div className="Container">
+        {this.state.movies.slice(2, this.state.movies.length).map((movie) => {
+          return (
+            <Movies
+              name={movie.name}
+              runtime={movie.runtimeInMinutes}
+              score={movie.rottenTomatesScore}
+              budget={movie.budgetInMillions}
+              revenue={movie.boxOfficeRevenueInMillions}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
